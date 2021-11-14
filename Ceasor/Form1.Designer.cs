7074c2f6aa919f7d93c5b56cda09a270799f7a2c
karim <karimabdelghany753@gmail.com>
2021-11-14 12:50:35 +0200

    finish

---
+++ Ceasor/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Ceasor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.TextBox();
+            this.key = new System.Windows.Forms.TextBox();
+            this.encText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.decText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(107, 329);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text
+            // 
+            this.text.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.text.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text.Location = new System.Drawing.Point(178, 21);
+            this.text.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(297, 38);
+            this.text.TabIndex = 1;
+            this.text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // key
+            // 
+            this.key.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.key.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key.Location = new System.Drawing.Point(178, 98);
+            this.key.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(95, 38);
+            this.key.TabIndex = 1;
+            this.key.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // encText
+            // 
+            this.encText.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.encText.Enabled = false;
+            this.encText.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encText.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.encText.Location = new System.Drawing.Point(178, 179);
+            this.encText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.encText.Name = "encText";
+            this.encText.Size = new System.Drawing.Size(297, 38);
+            this.encText.TabIndex = 1;
+            this.encText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(44, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(50, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(44, 179);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Encryption";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(351, 329);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // decText
+            // 
+            this.decText.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.decText.Enabled = false;
+            this.decText.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decText.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.decText.Location = new System.Drawing.Point(178, 256);
+            this.decText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.decText.Name = "decText";
+            this.decText.Size = new System.Drawing.Size(297, 38);
+            this.decText.TabIndex = 1;
+            this.decText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(42, 256);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 29);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Decryption";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(733, 422);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.decText);
+            this.Controls.Add(this.encText);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caesar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.TextBox encText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox decText;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Ceasor/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ceasor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String txt = this.text.Text;
+            String keyText = this.key.Text;
+            int key;
+
+            while (int.TryParse(keyText, out key) == false)
+            {
+                MessageBox.Show("Invalid input in key");
+                this.key.Focus();
+                this.key.Text = "0";
+                key = 0;
+                break;
+            }
+            
+            String encryptedText(int num)
+            {
+                String cipherText = "";
+                int equation;
+                for (int x = 0; x < txt.Length; x++)
+                {
+                    equation = txt[x] + num;
+                    if (txt[x] >= 65 && txt[x] <= 90)
+                    {
+                        cipherText += 
+                                equation > 90 ? (char)(equation - 26) 
+                                : (char)equation;
+                    }
+                    else
+                    {
+                        cipherText +=
+                                equation > 122 ? (char)(equation - 26)
+                                : (char)equation;
+                    }
+                    //MessageBox.Show(text[i].ToString());
+                    //cipherText = text[i].ToString();
+                }
+                this.encText.Focus();
+                return cipherText;
+            }
+
+            this.encText.Text = encryptedText(key);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String text = this.text.Text;
+            String keyText = this.key.Text;
+            int key;
+
+            while (int.TryParse(keyText, out key) == false)
+            {
+                MessageBox.Show("Invalid input in key");
+                this.key.Focus();
+                this.key.Text = "0";
+                key = 0;
+                break;
+            }
+
+            String encryptedText(int num)
+            {
+                String cipherText = "";
+                int equ;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    equ = text[i] - num;
+                    if (text[i] >= 65 && text[i] <= 90)
+                    {
+                        cipherText +=
+                                equ < 65 ? (char)(equ + 26)
+                                : (char)equ;
+                    }
+                    else
+                    {
+                        cipherText +=
+                                equ< 97 ? (char)(equ + 26)
+                                : (char)equ;
+                    }
+                    //MessageBox.Show(text[i].ToString());
+                    //cipherText = text[i].ToString();
+                }
+                this.decText.Focus();
+                return cipherText;
+            }
+
+            this.decText.Text = encryptedText(key);
+        }
+    }
+}
